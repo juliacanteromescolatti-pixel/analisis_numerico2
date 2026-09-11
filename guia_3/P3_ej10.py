c6a569ee37186c04c36faa46ec3d219c4d389036
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-09-11 08:07:08 -0300

    uodate

--- guia_3/P3_ej10.py
+++ guia_3/P3_ej10.py
@@ -5,19 +5,19 @@ def graficar_transformacion_esfera(epsilons):
     # Generar la bola unidad B = {x in R^2 : ||x||_2 = 1}
     theta = np.linspace(0, 2 * np.pi, 300)
     # Puntos de la esfera unidad (circunferencia en R^2)
-    X = np.array([np.cos(theta), np.sin(theta)]) 
-
+    X = np.array([np.cos(theta), np.sin(theta)]) # Es la circunf unidad
+    # graficamos circunf unid
     plt.figure(figsize=(9, 7))
-    
+
     # Graficar la esfera unidad original
     plt.plot(X[0, :], X[1, :], 'k--', label='Esfera Unidad Orig. (Norma 2)', linewidth=2)
 
     # Graficar la transformación A(eps) * X para cada epsilon
     for eps in epsilons:
-        A_eps = np.array([[1.0, 1.0 - eps], [0.0, 1.0]])
-        
+        A_eps = np.array([[1.0, 1.0 - eps], [0.0, 1.0]]) # A de epsilon en el inciso (a)
+
         # Transformación de los puntos
-        Y = A_eps @ X  
+        Y = A_eps @ X # modifica la circunf unidad multiplicandola por el A de epsilon(Matriz)
         plt.plot(Y[0, :], Y[1, :], label=f'Transformación $\epsilon = {eps}$')
 
     plt.axhline(0, color='gray', linestyle=':', alpha=0.6)
@@ -29,6 +29,9 @@ def graficar_transformacion_esfera(epsilons):
     plt.ylabel('$x_2$')
     plt.legend()
     plt.show()
+
+#Esta función dibuja una línea horizontal que cruza todo el gráfico (el eje X de referencia o en el ejr Y).
+#Estas funciones sirven para escribir el título o etiqueta que aparece abajo (eje X) y al costado (eje Y) del cuadro del gráfico.
 
 # Ejecución para los valores de epsilon indicados en la consigna
 epsilons = [0.25, 0.125, 0.0625, 1e-5]
